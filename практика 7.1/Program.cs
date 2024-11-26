9e25c51f2c0cd85bc919566bcc00e321126467ab
suecidusa <suecidusaaa@gmail.com>
2024-11-27 00:39:49 +0900

    Initial commit

---
+++ практика 7.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class AbsoluteDifference
+{
+    public static int CalculateDifference(int n)
+    {
+        int difference = Math.Abs(n - 123);
+        if (n > 123)
+        {
+            return difference * 3;
+        }
+        return difference;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Введите целое число:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int result = CalculateDifference(n);
+        Console.WriteLine($"Результат: {result}");
+    }
+}
